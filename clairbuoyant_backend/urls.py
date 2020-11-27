f9bd09a00f8b5a1f4dd6fe0f0fed3932ebb92a9a
kylejb <47502769+kylejb@users.noreply.github.com>
2020-11-27 00:12:13 -0500

    minor docstring formatting

--- clairbuoyant_backend/urls.py
+++ clairbuoyant_backend/urls.py
@@ -1,4 +1,4 @@
-"""clairbuoyant_backend URL Configuration
+"""clairbuoyant_backend URL Configuration.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
