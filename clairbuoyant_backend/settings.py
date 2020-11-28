4be1a657f326aa9e1897c0f221e044f1f9da0e22
kylejb <47502769+kylejb@users.noreply.github.com>
2020-11-27 21:13:14 -0500

    GeoDjango

--- clairbuoyant_backend/settings.py
+++ clairbuoyant_backend/settings.py
@@ -41,6 +41,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'buoyant.apps.BuoyantConfig',
+    'django.contrib.gis',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
