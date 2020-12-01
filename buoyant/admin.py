42d90db677f829df463b0f093511b28b98374775
kylejb <47502769+kylejb@users.noreply.github.com>
2020-11-30 23:50:43 -0500

    Registered models to administration dashboard.

--- buoyant/admin.py
+++ buoyant/admin.py
@@ -1,3 +1,9 @@
+from buoyant.models import Buoy, Observation
 from django.contrib import admin
 
-# Register your models here.
+
+class BuoyantAdmin(admin.ModelAdmin): ...
+
+
+admin.site.register(Buoy, BuoyantAdmin)
+admin.site.register(Observation, BuoyantAdmin)
