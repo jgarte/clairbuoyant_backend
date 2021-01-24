e48a20ffd4c52250b7763959a3ed172c62218c95
kylejb <47502769+kylejb@users.noreply.github.com>
2021-01-24 14:50:01 -0500

    Admin command to scrape buoys.

---
+++ buoyant/management/commands/get_buoys.py
@@ -0,0 +1,10 @@
+from buoyant.services import NDBCScraper
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    help = "Collect buoy data."
+
+    def handle(self, *args, **kwargs):
+        NDBCScraper()
+        self.stdout.write("Task complete.")
