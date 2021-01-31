bfdfacc4659d8f5c34ed9cf601ca96ab3a684137
kylejb <47502769+kylejb@users.noreply.github.com>
2021-01-30 19:29:55 -0500

    Added Meteorological model to admin dashboard.

--- buoyant/admin.py
+++ buoyant/admin.py
@@ -1,8 +1,10 @@
-from buoyant.models import Buoy
+from buoyant.models import Buoy, Meteorological
 from django.contrib import admin
 
 
-class BuoyantAdmin(admin.ModelAdmin): ...
+class BuoyantAdmin(admin.ModelAdmin):
+    ...
 
 
 admin.site.register(Buoy, BuoyantAdmin)
+admin.site.register(Meteorological, BuoyantAdmin)
