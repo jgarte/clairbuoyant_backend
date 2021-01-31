70f6834377df9385a50ade963a0583a269b8b70a
kylejb <47502769+kylejb@users.noreply.github.com>
2021-01-30 21:50:49 -0500

    [WIP] Testing Meteorological serialization & views

--- buoyant/serializers.py
+++ buoyant/serializers.py
@@ -1,5 +1,26 @@
-from buoyant.models import Buoy
+from buoyant.models import Buoy, Meteorological
 from rest_framework import serializers
+
+
+class MeteorologicalSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Meteorological
+        fields = [
+            "observation_date",
+            "wind_speed",
+            "wind_gust",
+            "wave_height",
+            "dom_wave_period",
+            "avg_wave_period",
+            "wave_dir",
+            "sea_pressure",
+            "air_temp",
+            "sea_temp",
+            "tide",
+            "dewpoint_temp",
+            "station_visibility",
+            "pressure_tendency",
+        ]
 
 
 class BuoySerializer(serializers.ModelSerializer):
--- clairbuoyant_backend/urls.py
+++ clairbuoyant_backend/urls.py
@@ -1,14 +1,15 @@
 """clairbuoyant_backend URL Configuration."""
-from buoyant.views import BuoyView
+from buoyant.views import BuoyView, MetView
 from django.conf.urls import url
 from django.contrib import admin
 from rest_framework import routers
 
 router = routers.SimpleRouter()
-router.register(r'buoys', BuoyView)
+router.register(r"buoys", BuoyView)
+router.register(r"met", MetView)
 
 urlpatterns = [
-    url('admin/', admin.site.urls),
+    url("admin/", admin.site.urls),
 ]
 
 urlpatterns += router.urls
